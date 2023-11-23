e2d1a867e7efdb614e34807bf62e8ca6602779f7
bunny1137 <lokhandemayu123@gmail.com>
2023-11-23 18:12:19 +0530

    created

---
+++ area_circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 13:19:11 2023
+
+@author: Asus
+"""
+
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
